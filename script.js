a074ca0d09a32cf516f6607963825f17983b4abd
RUDHRESH M <rudhreshm191106@gmail.com>
2025-04-07 10:59:25 +0530

    Token Created

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+const jwt = require('jsonwebtoken');
+const secretKey = 'mySecetKey'
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
-}
+  return jwt.sign(payload, secretKey);
+};
 
 const decrypt = (token) => {
   // return decoded payload
+  return jwt.verify(token, secretKey);
+};
+
+const payload = {username: 'john_doe'};
+
+const token = encrypt(payload);
+console.log('Token:', token);
+
+const data  = decrypt(token);
+
+if(data && data.username === payload.username) {
+  console.log('Success:', data);
+} else {
+  console.log('Failed to verify token');
 }
 
 module.exports = {
